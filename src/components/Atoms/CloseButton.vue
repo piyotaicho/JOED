6183ced86cc8e0b346f1300661116384e5d2c40e
unknown <mohyama@mohnet.com>
2020-10-21 00:18:21 +0900

    New

---
+++ src/components/Atoms/CloseButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="closebutton" @click="Close">&#xe6db;</div>
+</template>
+
+<script>
+export default {
+  name: 'CloseButton',
+  methods: {
+    Close () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+<style lang="sass">
+div.closebutton
+  position: absolute
+  right: 1rem
+  top: 1rem
+  width: 1rem
+  height: 1rem
+  font-family: 'element-icons'
+  font-size: 1.2rem
+  &:hover
+    font-weight: bold
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <CloseButton @close="Close"></CloseButton>
+    <div class="dashboard">
+      <div class="dashboard-title" ref="title">{{InstituteName}}</div>
+      <div class="dashboard-row">
+        <span>{{MasterYear}}年版マスタ</span>
+        <span>表示 {{ViewCount}}件 / 全 {{TotalCount}}件</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CloseButton from '@/components/Atoms/CloseButton'
+import DiagnosisMaster from '@/modules/DiagnosisItemList'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    CloseButton
+  },
+  created () {
+    const master = new DiagnosisMaster()
+    this.MasterYear = master.getMasterYear()
+  },
+  data () {
+    return ({
+      MasterYear: '0000',
+      TotalCount: 0
+    })
+  },
+  computed: {
+    InstituteName () {
+      return this.$store.getters['system/InstitutionName'] || '(施設名称未設定)'
+    },
+    ViewCount () {
+      return this.$store.getters.NumberOfCases
+    }
+  },
+  methods: {
+    Close () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+div.dashboard
+  margin: 0.7rem
+  margin-right: 2.8rem
+  padding: 0.3rem
+  border: 0.18rem solid var(--border-color-base)
+  border-radius: 0.3rem
+
+div.dashboard-title
+  margin: 0.3rem 0
+  font-size: 1.3rem
+  line-height: 1.8rem
+  font-weight: bold
+  letter-spacing: 0.08rem
+  overflow-wrap: break-word
+  word-break: break-word
+
+div.dashboard-row
+  display: flex
+  flex-direction: row
+  justify-content: space-around
+  font-size: 0.95rem
+</style>
