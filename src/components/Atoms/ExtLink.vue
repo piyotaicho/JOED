deb4db754353fcbdba9a491cd6072dd3cbb3d8e9
unknown <mohyama@mohnet.com>
2020-10-24 00:12:05 +0900

    New to open links in a system default browser

---
+++ src/components/Atoms/ExtLink.vue
@@ -0,0 +1,31 @@
+<template>
+  <span class="extlink" @click="Click" style="text-decoration: underline;">
+    <slot>{{url}}</slot>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'ExtLink',
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    Click () {
+      if (process.env.VUE_APP_MODE === 'electron') {
+        try {
+          const { shell } = require('electron')
+          shell.openExternal(this.url)
+        } catch (_) {
+          // noop
+        }
+      } else {
+        window.open(this.url, '_blank')
+      }
+    }
+  }
+}
+</script>
