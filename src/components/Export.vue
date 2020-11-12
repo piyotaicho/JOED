1a7a2e79bd1c26c5f5e3d885358dc55509956b70
unknown <mohyama@mohnet.com>
2020-11-12 23:52:34 +0900

    Export/import move to non routerview Dynamic loading implimented

---
+++ src/components/Export.vue
@@ -0,0 +1,394 @@
+<template>
+  <div class="utility">
+    <div class="utility-switches">
+      <div>
+        <div class="label">出力する年次</div>
+        <SelectYear class="field" v-model="exportYear" :accept-all="false"/>
+      </div>
+
+      <InputSwitchField
+        v-model="allFields"
+        title="出力する様式"
+        :options="{学会提出データ: false, バックアップデータ: true}"/>
+
+      <InputSwitchField
+        v-model="exportRaw"
+        :disabled="!exportAllFields"
+        title="データ形式"
+        :options="{生データ: true, 処理データ: false}"/>
+
+      <InputSwitchField
+        v-model="addHeader"
+        :disabled="!(exportAllFields && exportRaw)"
+        title="ヘッダの生成"
+        :options="{しない: false, する: true}"/>
+
+      <div>
+        <el-button type="primary" @click="Process()" :disabled="exportYear=='' || processing">出力データの作成</el-button>
+      </div>
+    </div>
+
+    <el-collapse-transition>
+      <div class="progress-views" v-show="processStep !== undefined">
+        <el-steps :active="processStep" process-status="warning" finish-status="success" direction="vertical" space="42px">
+          <el-step title="システム設定の確認"/>
+          <el-step title="読み込み症例の確認を検証"/>
+          <el-step title="登録内容の妥当性の検証">
+            <template #description>
+              <el-progress :v-show="progressCheckConsistency > 0" :percentage="progressCheckConsistency"/>
+            </template>
+          </el-step>
+          <el-step title="提出用データとして整形">
+            <template #description>
+              <el-progress :v-show="progressCreateData > 0" :percentage="progressCreateData"/>
+            </template>
+          </el-step>
+          <el-step title="チェックサムの計算とヘッダの付与"/>
+        </el-steps>
+      </div>
+    </el-collapse-transition>
+
+    <el-collapse-transition>
+      <div v-show="readyToExport">
+        <el-dropdown split-button type="primary" @click="Download()">
+          出力先を指定してファイルの出力
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item @click.native="showPreview = true">出力先結果の確認</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-collapse-transition>
+
+    <TheWrapper v-if="processing"/>
+    <TheWrapper v-if="showPreview" alpha="60" @click="showPreview = false">
+      <div id="preview">
+        画面のクリックで戻ります.
+        <hr/>
+        <pre>{{exportText}}</pre>
+      </div>
+    </TheWrapper>
+  </div>
+</template>
+
+<script>
+import InputSwitchField from '@/components/Molecules/InputSwitchField'
+import SelectYear from '@/components/Molecules/SelectYear'
+import TheWrapper from '@/components/Atoms/TheWrapper'
+import CaseDocumentHandler from '@/modules/DbItemHandler'
+import * as Popups from '@/modules/Popups'
+import HHX from 'xxhashjs'
+import { ValidateCase } from '@/modules/CaseValidater'
+
+export default {
+  name: 'Export',
+  components: {
+    SelectYear, InputSwitchField, TheWrapper
+  },
+  data () {
+    return ({
+      exportYear: '',
+      exportAllFields: false,
+      exportRaw: true,
+      addHeader: false,
+
+      exportText: '',
+
+      processing: false,
+      processStep: undefined,
+      progressCheckConsistency: 0,
+      progressCreateData: 0,
+
+      showPreview: false
+    })
+  },
+  watch: {
+    exportYear () {
+      this.ResetState()
+    },
+    exportAllFields () {
+      this.ResetState()
+    },
+    exportRaw () {
+      this.ResetState()
+    },
+    addHeader () {
+      this.ResetState()
+    }
+  },
+  computed: {
+    allFields: {
+      get () {
+        return this.exportAllFields
+      },
+      async set (newvalue) {
+        if (newvalue && await Popups.confirm('不用意に全てのフィールドのデータを出力するのは,個人情報保護の観点からお薦め出来ません.\nそれでも出力しますか?')) {
+          this.exportAllFields = true
+        } else {
+          this.exportAllFields = false
+        }
+      }
+    },
+    baseQuery () {
+      const query = {
+        DocumentId: { $gt: 0 }
+      }
+      if (this.exportYear !== '') {
+        const reg = new RegExp('^' + this.exportYear + '-')
+        query.DateOfProcedure = { $regex: reg }
+      }
+      return query
+    },
+    readyToExport () {
+      return this.exportText.length > 4
+    }
+  },
+  methods: {
+    ResetState () {
+      this.processStep = undefined
+      this.exportText = ''
+    },
+
+    async Process () {
+      if (this.processing) return
+
+      this.processStep = 0
+      this.progressCheckConsistency = 0
+      this.progressCreateData = 0
+      this.exportText = ''
+      this.showPreview = false
+
+      try {
+        await this.CheckSystem()
+        this.processStep++
+
+        await this.CheckImported()
+        this.processStep++
+
+        const documentIds = await this.CheckConsistency()
+        this.processStep++
+
+        const exportitems = await this.CreateExportData(documentIds)
+        this.processStep++
+
+        this.exportText = await this.CreateHeader(exportitems)
+        this.processStep++
+      } catch (error) {
+        await this.$nextTick()
+        Popups.error(error.message)
+      } finally {
+        await this.$nextTick()
+        this.processing = false
+      }
+    },
+
+    async Download () {
+      if (!this.exportAllFields ||
+        (
+          await Popups.confirmYesNo('保存されるデータにはID番号・氏名・年齢などの個人情報が含まれている可能性があります.\n\n処理を続行しますか?') &&
+          await Popups.confirm('出力されたファイルの取り扱いは厳重行ってください.')
+        )
+      ) {
+        // ブラウザの機能でダウンロードさせる.
+        const temporaryElementA = document.createElement('A')
+        temporaryElementA.href = URL.createObjectURL(new Blob([this.exportText]), { type: 'application/json' })
+        temporaryElementA.download = 'joed-export-data.json'
+        temporaryElementA.style.display = 'none'
+        document.body.appendChild(temporaryElementA)
+        temporaryElementA.click()
+        document.body.removeChild(temporaryElementA)
+      }
+    },
+
+    // Step 1 - 施設名とIDが設定されているかを確認
+    //
+    async CheckSystem () {
+      if (!this.$store.getters['system/InstitutionID']) {
+        throw new Error('施設情報が未設定です.')
+      }
+
+      const countJSOGId = await this.$store.dispatch('dbCount', {
+        Query:
+          {
+            JSOGId: { $exists: true },
+            ...this.baseQuery
+          }
+      })
+      if (countJSOGId > 0 && !this.$store.getters['system/JSOGInstitutionID']) {
+        throw new Error('日本産科婦人科学会腫瘍登録施設番号が未設定です.')
+      }
+    },
+
+    // Step 2 - インポートデータがすべて確認されているかを確認
+    //
+    // インポートデータ( Imported )で特になんの問題も無くインポートできたもの以外には Notification がある
+    async CheckImported () {
+      const count = await this.$store.dispatch('dbCount', {
+        Query:
+          {
+            Imported: { $exists: true },
+            ...this.baseQuery
+          }
+      })
+      if (count > 0) {
+        throw new Error('未確認の読み込み症例が ' + count + ' 症例あります.\n確認を御願いします.')
+      }
+    },
+    // Step 3 - データの妥当性検証
+    //
+    // 基本的に入力時に検証されているので大丈夫だと思うが：
+    //  必須項目の有無
+    //  項目の重複(ditto含む)
+    async CheckConsistency () {
+      this.progressCheckConsistency = 0
+
+      const documentIds = (
+        await this.$store.dispatch('dbFind',
+          {
+            Query: this.baseQuery,
+            Projection: { DocumentId: 1, _id: 0 }
+          }) ||
+        []
+      ).map(item => item.DocumentId)
+      if (documentIds.length === 0) throw new Error('エクスポートの対象症例がありません.')
+
+      let errorCount = 0
+
+      for (const index in documentIds) {
+        this.progressCheckConsistency = parseInt(index * 100.0 / documentIds.length)
+        try {
+          await ValidateCase(
+            await this.$store.dispatch('dbFindOne',
+              {
+                Query: { DocumentId: documentIds[index] }
+              })
+          )
+        } catch (error) {
+          this.$store.dispatch('dbUpdate',
+            {
+              Query: { DocumentId: documentIds[index] },
+              Update: { $set: { Notification: error } }
+            })
+          errorCount++
+        }
+      }
+      this.progressCheckConsistency = 100
+
+      if (errorCount > 0) {
+        throw new Error(
+          'データ検証で' + errorCount + '件のエラーが確認されました.\n' +
+          '修正を御願いします.'
+        )
+      }
+
+      return documentIds
+    },
+
+    // Step 4 - データの整形
+    //
+    async CreateExportData (documentIds) {
+      this.progressCreateData = 0
+      const ExportItems = []
+      const hashHHX = HHX.h64(this.$store.getters['system/SALT'])
+
+      // let serial = this.forceRenumber ? 0
+      //   : Number(
+      //     (
+      //       await this.$store.dispatch('dbFindOne',
+      //         {
+      //           Query:
+      //             {
+      //               UniqueID: { $exists: true },
+      //               ...this.baseQuery
+      //             },
+      //           Sort: { UniqueID: -1 }
+      //         }) ||
+      //         {
+      //           // UniqueIDが無い場合のダミー つまるところ 0.
+      //           UniqueID: '2222-99001-0'
+      //         }
+      //     ).UniqueID.substr(11)
+      //   )
+
+      for (const index in documentIds) {
+        this.progressCreateData = parseInt(index * 100.0 / documentIds.length)
+        const exportdocument = await this.$store.dispatch('dbFindOne',
+          {
+            Query: { DocumentId: documentIds[index] },
+            Projection: { _id: 0 }
+          }
+        )
+        // if (this.forceRenumber || !exportdocument.UniqueID) {
+        //   ++serial
+        //   exportdocument.UniqueID = [
+        //     this.$store.getters['system/InstitutionID'],
+        //     this.exportYear,
+        //     serial
+        //   ].join('-')
+
+        //   await this.$store.dispatch('dbUpdate',
+        //     {
+        //       Query: { DocumentId: documentIds[index] },
+        //       Update: { $set: { UniqueID: exportdocument.UniqueID } }
+        //     })
+        // }
+        if (this.exportAllFields && this.exportRaw) {
+          ExportItems.push(exportdocument)
+        } else {
+          ExportItems.push(
+            {
+              ...CaseDocumentHandler.ExportCase(
+                exportdocument,
+                {
+                  exportAllFields: this.exportAllFields
+                }
+              ),
+              hash: hashHHX.update(JSON.stringify(exportdocument)).digest().toString(36)
+            }
+          )
+        }
+      }
+      this.progressCreateData = 100
+      return ExportItems
+    },
+
+    // Step 5 - データヘッダの作成
+    //
+    async CreateHeader (exportItem) {
+      if (!(this.exportAllFields && this.exportRaw && !this.addHeader)) {
+        const length = exportItem.length
+
+        if (length > 0) {
+          const TimeStamp = Date.now()
+
+          const exportText = JSON.stringify(exportItem, ' ', 2)
+          const hash = HHX.h64(exportText, TimeStamp).toString(16)
+
+          exportItem.unshift({
+            InstitutionName: this.$store.getters['system/InstitutionName'],
+            InstitutionID: this.$store.getters['system/InstitutionID'],
+            JSOGoncologyboardID: this.$store.getters['system/JSOGInstitutionID'],
+            TimeStamp: TimeStamp.toString(10),
+            Year: this.exportYear || 'ALL',
+            NumberOfCases: exportItem.length,
+            Version: this.$store.getters['system/ApplicationVersion'],
+            hash: hash
+          })
+        }
+      }
+      return JSON.stringify(exportItem, ' ', 2)
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+div#preview
+  position: relative
+  height: 80vh
+  overflow-y: scroll
+  margin: 4rem
+  padding: 1rem
+  border: 2px solid var(--color-text-primary)
+  border-radius: 0.5rem
+  background: var(--background-color-list)
+</style>
---
+++ src/components/Import.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="utility">
+    <div>
+      <div class="title">mergeファイルからのデータ読み込み</div>
+      <div>
+        症例登録システムver.4で生成されたmergeファイルから症例データを読み込むことが出来ます.<br/>
+        システムの大幅な変更に伴い,データの修正は必ず必要になります.併せて以下の制限がありますがご了承ください.<br/>
+        <ul>
+          <li>この処理で読み込まれたデータについては, 全て編集と確認が必要になります.</li>
+          <li>合併症については「合併症なし」以外は自動での読み込みが出来ません. ご面倒ですが個々に入力を御願いします.</li>
+        </ul>
+      </div>
+    </div>
+    <div>
+      <InputFile @change="LoadFile"></InputFile>
+      <el-button type="primary" :disabled="InFile.length <= 0 || Processing" @click="ProcessFile()">読み込みを開始</el-button>
+      <el-button type="primary" :disabled="!ReadyToRegister || Processing" :loading="Processing" @click="CommitImported">読み込んだデータの登録</el-button>
+    </div>
+
+    <el-collapse-transition>
+      <div class="progress-views" v-show="ProcessStep">
+        <el-steps :active="ProcessStep" process-status="warning" finish-status="success" direction="vertical" space="42px">
+          <el-step title="入力ファイルのフォーマット検証">
+            <template #description>
+              <span>ファイル中に{{InFile.length}}件のデータがあります.<br/></span>
+              <span v-if="DeIdentified">指定のファイルは提出用データです.患者IDは登録番号から自動生成されます.</span>
+            </template>
+          </el-step>
+          <el-step title="フィールドの割り当てとレコードの検証">
+            <template #description>
+              <span v-if="QueryDocuments.length > 0">{{QueryDocuments.length}}件のデータが対象になります.</span>
+            </template>
+          </el-step>
+          <el-step title="登録">
+            <template #description>
+              <el-progress  v-show="Processing" :percentage="ImportProgress"></el-progress>
+            </template>
+          </el-step>
+        </el-steps>
+      </div>
+    </el-collapse-transition>
+
+    <TheWrapper v-if="Processing"/>
+  </div>
+</template>
+
+<script>
+import InputFile from '@/components/Molecules/InputFile'
+import TheWrapper from '@/components/Atoms/TheWrapper'
+import { phraseTitledCSV, CreateDocument } from '@/modules/CSVimporter'
+import * as Popups from '@/modules/Popups'
+
+export default {
+  name: 'Import',
+  components: { InputFile, TheWrapper },
+  data () {
+    return ({
+      Processing: false,
+      ProcessStep: 0,
+      ImportProgress: 0,
+      InFile: [],
+      DeIdentified: false,
+      ReadyToRegister: false,
+      QueryDocuments: []
+    })
+  },
+  methods: {
+    ResetState () {
+      this.Processing = false
+      this.ProcessStep = 0
+      this.ReadyToRegister = false
+      this.InFile.splice(0)
+      this.QueryDocuments.splice(0)
+    },
+    LoadFile (eventvalue) {
+      const records = phraseTitledCSV(eventvalue)
+      this.InFile.splice(0)
+      if ((Array.isArray(records) && records.length > 0) &&
+        (records[0]['内部ID'] && records[0]['手術時間'])) {
+        this.DeIdentified = !records[0].ID
+        Object.assign(this.InFile, records)
+      } else {
+        Popups.alert('指定されたファイルは 症例登録システムJOE-D version 4 から適切に出力されたmergeファイル(.mer)ではありません.')
+        this.ResetState()
+      }
+      this.ProcessStep = 0
+    },
+    async ProcessFile () {
+      this.QueryDocuments.splice(0)
+      try {
+        this.ProcessStep = 1
+        for (const record of this.InFile) {
+          try {
+            const createddocument = CreateDocument(record)
+            this.QueryDocuments.push(createddocument)
+          } catch (error) {
+            if (!(await Popups.confirm('指定されたファイル中に不適切なレコードがあります.\n残りの処理を続行しますか?'))) {
+              throw new Error('不適切なレコード\n', JSON.stringify(record))
+            }
+          }
+        }
+        this.ProcessStep = 2
+        this.ReadyToRegister = true
+      } catch (error) {
+        this.ResetState()
+      }
+    },
+    async CommitImported () {
+      this.ProcessStep = 3
+      this.ImportProgress = 0
+      this.Processing = true
+      let count = 0
+      const errors = []
+
+      for (const newdocument of this.QueryDocuments) {
+        await this.$store.dispatch('UpsertDocument', newdocument)
+          .then(_ => { this.ImportProgress = Math.round(++count * 100 / this.QueryDocuments.length) })
+          .catch(error => errors.push(error.Message || error + '\n' + JSON.stringify(newdocument)))
+      }
+
+      this.ImportProgress = 100
+      await this.$nextTick()
+
+      let message = count + ' 例を登録しました.'
+      if (errors.length > 0) {
+        message += '\n' + errors.length + ' 件の登録に失敗しました.(重複登録の可能性があります.)'
+        Popups.alert(message)
+      } else {
+        Popups.information(message)
+      }
+
+      // ステータスをリセットする
+      this.ResetState()
+    }
+  }
+}
+</script>
