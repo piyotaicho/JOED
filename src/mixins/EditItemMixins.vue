50334b88dc38b2ffef920a82d3628693bebbe06e
Yasuhiro Yamamoto <mohyama@mohnet.com>
2021-01-29 23:39:26 +0900

    Reduce use of mixin

--- src/mixins/EditItemMixins.vue
+++ src/mixins/EditItemMixins.vue
@@ -13,30 +13,9 @@ export default {
       default: ''
     }
   },
-  data () {
-    return {
-      Category: '',
-      TargetOrgan: '',
-      SelectedItem: '',
-      EditableItem: ''
-    }
-  },
   computed: {
     UserEditingAllowed () {
-      return !!this.Category && !this.SelectedItem
-    },
-
-    TrimmedEditableItem () {
-      const enteredValue = this.EditableItem.toString().replace(/^[\s|\u3000]+$/g, '')
-      return enteredValue
-    },
-
-    IsReadyToCommit () {
-      return (this.TrimmedEditableItem !== '')
-    },
-
-    IsItemEdited () {
-      return this.SelectedItem !== this.EditableItem
+      return !!this.category && !this.selectedItem
     }
   },
   methods: {
@@ -44,23 +23,30 @@ export default {
       return str[str.length - 1] !== '$' ? str : str.substr(0, str.length - 1)
     },
 
-    ToggleEditsection () {
-      this.ExpandEditsection = !this.ExpandEditsection || (this.ItemValue.UserTyped === true) || this.IsItemEdited
-    },
-
     async CategoryIsChanged () {
-      this.TargetOrgan = ''
-      if (this.SelectedItem !== '') {
-        this.EditableItem = ''
+      this.target = ''
+      if (this.selectedItem !== '') {
+        this.freewordText = ''
       }
 
-      this.SelectedItem = ''
-      this.CandidateItems.splice(0)
+      this.selectedItem = ''
+      if (this.CandidateItems) {
+        this.CandidateItems.splice(0)
+      }
+
+      if (this.Description) {
+        this.Description.Title = ''
+      }
+
+      if (this.AdditionalProcedure) {
+        this.AdditionalProcedure.Title = ''
+      }
 
       await this.$nextTick()
 
+      // 対象臓器が1つだけのときはそれを選択する
       if (this.TargetOrgans.length === 1) {
-        this.TargetOrgan = this.TargetOrgans[0]
+        this.target = this.TargetOrgans[0]
         await this.$nextTick()
 
         this.SetCandidateItemsBySelection()
