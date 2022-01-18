335d8751584308067a325fe863ed6e2b082828a5
Yasuhiro Yamamoto <mohyama@mohnet.com>
2022-01-19 01:56:29 +0900

    Fix setting routines

--- src/store/modules/system.js
+++ src/store/modules/system.js
@@ -17,34 +17,43 @@ export default {
           DocumentId: -1
         }
       },
+      UnlockExportJSOGId: false,
+      UnlockExportNCDId: false,
       CSVruleset: '{}',
       ShowStartupDialog: true
     },
     StartupDialogStatus: true
   },
   getters: {
+    // システム固有のソルト値
     SALT (state) {
       return state.settings.Salt
     },
+    // APP_NAMEの中継
     ApplicationName () {
       return process.env.VUE_APP_NAME
     },
+    // APP_VERSIONの中継
     ApplicationVersion () {
       return process.env.VUE_APP_VERSION
     },
+    // Vueのバージョンの中継
     VueVersion () {
       return Vue.version || 'undefined'
     },
+    // 実行プラットフォーム(v1.2からブラウザ版はアーキテクチャを返さない)
     Platform () {
       return process.env.VUE_APP_ELECTRON
         ? process.platform
-        : (window.navigator.platform.includes('Mac') ? 'darwin' : 'win32')
+        : 'browser'
     },
 
+    // settingsオブジェクト全体
     Settings (state) {
       return state.settings
     },
 
+    // 施設情報オブジェクト
     InstituteInformation (state) {
       return {
         InstitutionName: state.settings.InstitutionName,
@@ -52,36 +61,55 @@ export default {
         JSOGoncologyboardID: state.settings.JSOGoncologyboardID
       }
     },
+    // 施設名
     InstitutionName (state) {
       return state.settings.InstitutionName
     },
+    // 施設番号
     InstitutionID (state) {
       return state.settings.InstitutionID
     },
+    // 日産婦腫瘍登録施設番号
     JSOGInstitutionID (state) {
       return state.settings.JSOGoncologyboardID
     },
 
+    // 日産婦腫瘍登録番号の編集可否
     EditJSOGId (state) {
       return state.settings.EditJSOGId
     },
+    // NCD登録番号の編集可否
     EditNCDId (state) {
       return state.settings.EditNCDId
     },
 
+    // 日産婦腫瘍登録番号の生データ出力可否
+    ExportJSOGId (state) {
+      return state.settings.UnlockExportJSOGId
+    },
+    // NCD登録番号の生データ出力可否
+    ExportNCDId (state) {
+      return state.settings.UnlockExportNCDId
+    },
+
+    // 起動時のメッセージダイアログ表示の有無
     ShowStartupDialog (state) {
       return state.StartupDialogStatus
     },
 
+    // 表示設定の保存値
     SavedView (state) {
       return state.settings.View
     },
-
+    // CSVインポートのルール保存値
     SavedCSVrule (state) {
       return state.settings.CSVruleset
     }
   },
   mutations: {
+    // settingsにオブジェクトから値をコピー
+    //
+    // @param {object}
     SetPreferences (state, payload = {}) {
       for (const key in payload) {
         if (state.settings[key] !== undefined) {
@@ -89,6 +117,9 @@ export default {
         }
       }
     },
+    // 表示設定を保存
+    //
+    // @param {object}
     SetView (state, payload = {}) {
       const newView = {}
       if (payload.Filters) {
@@ -109,40 +140,40 @@ export default {
         newView
       ))
     },
+    // 起動時のメッセージダイアログ表示フラグ設定
+    //
+    // @param{boolean}
     CloseStartupDialog (state) {
       state.StartupDialogStatus = false
     }
   },
   actions: {
-    LoadPreferences (context) {
-      return LoadConfig(context)
-        .then(settingdocument => {
-          const Settings =
-            {
-              View: {
-                Filters: undefined,
-                Sort: undefined
-              },
-              ...(settingdocument ? settingdocument.Settings : {})
-            }
+    // 設定(ファイル)から設定を読み込みストアに反映する
+    //
+    async LoadPreferences (context) {
+      const settings = (await LoadConfig(context))?.Settings || {}
 
-          if (Settings) {
-            context.commit('SetPreferences', Settings)
-            context.commit('SetFilters', Settings.View.Filters, { root: true })
-            context.commit('SetSort', Settings.View.Sort, { root: true })
-          }
-          if (Settings.ShowStartupDialog === false) {
-            context.commit('CloseStartupDialog')
-          }
-        })
+      context.commit('SetPreferences', settings)
+      context.commit('SetFilters', settings?.View?.Filters, { root: true })
+      context.commit('SetSort', settings?.View?.Sort, { root: true })
+
+      if (settings?.ShowStartupDialog === false) {
+        context.commit('CloseStartupDialog')
+      }
     },
+    // 設定(ファイル)にストアの状況を保存する
+    //
     SavePreferences (context) {
       return SaveConfig(context.state.settings, context)
     },
+    // 起動時のメッセージダイアログ表示を設定・保存する
+    //
     async SetAndSaveShowStartupDialog (context, value) {
       context.commit('SetPreferences', { ShowStartupDialog: !!value })
       await context.dispatch('SavePreferences')
     },
+    // 現在の表示設定を保存する
+    //
     async SaveCurrentView (context) {
       context.commit('SetView', {
         Filters: context.rootState.Filters,
