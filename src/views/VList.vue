1c41bed7cc5dc6e3ca651be519941edbf09e0fd5
Yasuhiro Yamamoto <mohyama@mohnet.com>
2025-08-13 00:41:53 +0900

    VListのローディング表示をelement-plusに実装変更 vue-infinite-loadingを削除

--- src/views/VList.vue
+++ src/views/VList.vue
@@ -2,12 +2,13 @@
 import { onMounted, ref, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useStore } from '@/store'
+import { Loading } from '@element-plus/icons-vue'
 import DrawerButton from '@/components/Atoms/DrawerButton.vue'
 import NewEntryButton from '@/components/Atoms/NewEntryButton'
 import CaseDocument from '@/components/Organisms/CaseDocument'
 import ListDrawer from '@/components/Organisms/ListDrawer'
 import WelcomeBanner from '@/components/Organisms/WelcomeBanner'
-import InfiniteLoading from 'vue-infinite-loading'
+// import InfiniteLoading from 'vue-infinite-loading' // Vue 3非対応のため削除
 
 const store = useStore()
 const route = useRoute()
@@ -30,7 +31,9 @@ const uids = computed(() => store.getters.PagedUids)
 
 const isShowStartupDialog = computed(() => store.getters['system/ShowStartupDialog'])
 
-const displayIdentifier = computed(() => store.getters.DisplayIdentifier)
+// Element Plus infinite scroll用の状態管理
+const loading = ref(false)
+const noMore = computed(() => store.getters.PagedUidsRange >= store.getters.NumberOfCases)
 
 const createNewEntry = () => router.push({ name: 'edit', params: { uid: 0 } })
 
@@ -38,13 +41,16 @@ const openDrawer = () => { showMenuDrawer.value = true }
 
 const closeDrawer = () => { showMenuDrawer.value = false }
 
-const handleInfinite = (state) => {
-  store.commit('IncrementDocumentListRange')
-  if (store.getters.PagedUidsRange >= store.getters.NumberOfCases) {
-    state.complete()
-  } else {
-    state.loaded()
-  }
+// Element Plus infinite scroll のハンドラー
+const loadMore = () => {
+  if (loading.value || noMore.value) return
+
+  loading.value = true
+  // 少し遅延を入れてユーザー体験を改善
+  setTimeout(() => {
+    store.commit('IncrementDocumentListRange')
+    loading.value = false
+  }, 100)
 }
 
 const moveFocus = (offset) => {
@@ -67,7 +73,10 @@ const moveFocus = (offset) => {
     @keydown.down.prevent="moveFocus(+1)"
     @keydown.j="moveFocus(+1)"
   >
-    <div class="itemlist">
+    <div class="itemlist"
+         v-infinite-scroll="loadMore"
+         :infinite-scroll-disabled="loading || noMore"
+         :infinite-scroll-distance="200">
       <DrawerButton class="open-drawer" tab-index="0" @click="openDrawer"/>
       <NewEntryButton class="list-new-entry" tab-index="0" @click="createNewEntry"/>
 
@@ -76,10 +85,19 @@ const moveFocus = (offset) => {
       <template v-for="uid in uids" :key="uid">
         <CaseDocument :uid="uid"/>
       </template>
-      <InfiniteLoading @infinite="handleInfinite" :identifier="displayIdentifier" ref="infiniteloading">
-        <template v-slot:no-more><span></span></template>
-        <template v-slot:no-results><span></span></template>
-      </InfiniteLoading>
+
+      <!-- Element Plus Infinite Scroll用のローディング表示 -->
+      <div v-if="loading" class="loading-container">
+        <el-icon class="is-loading">
+          <Loading />
+        </el-icon>
+        <span>読み込み中...</span>
+      </div>
+
+      <!-- 全データロード完了表示（オプション） -->
+      <!-- <div v-if="noMore && uids.length > 0" class="no-more-container">
+        <span>すべてのデータを読み込みました</span>
+      </div> -->
     </div>
 
     <WelcomeBanner v-if="isShowStartupDialog"/>
@@ -101,6 +119,26 @@ div.itemlist
     height: 60px
     width: 1px
     content: ''
+
+// Element Plus infinite scroll用のスタイル
+.loading-container
+  display: flex
+  justify-content: center
+  align-items: center
+  padding: 20px
+  gap: 8px
+  color: #666
+
+  .el-icon
+    font-size: 18px
+
+.no-more-container
+  display: flex
+  justify-content: center
+  align-items: center
+  padding: 20px
+  color: #999
+  font-size: 14px
 
 div.open-drawer
   position: fixed
