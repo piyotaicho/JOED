35747ac425d572bf6677577a780dbf942f862133
Yasuhiro Yamamoto <mohyama@mohnet.com>
2025-09-06 02:45:40 +0900

    Viteのpolyfill問題を逐次処理

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { version, description } from './package.json'
+
+const isElectron = process.env.VITE_APP_ELECTION !== undefined
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -18,7 +22,46 @@ export default defineConfig({
           ? './src/modules/electron'
           : './src/modules/serve',
         import.meta.url)),
+      ...isElectron
+        ? {}
+        : {
+          util: 'util/',
+          events: 'events/',
+          process: 'process/browser',
+          './customUtils.js': fileURLToPath(
+            new URL(
+              './node_modules/@seald-io/nedb/browser-version/lib/customUtils.js',
+              import.meta.url
+            )
+          ),
+          './storage.js': fileURLToPath(
+            new URL(
+              './node_modules/@seald-io/nedb/browser-version/lib/storage.browser.js',
+              import.meta.url
+            )
+          ),
+          './byline': fileURLToPath(
+            new URL(
+              './node_modules/@seald-io/nedb/browser-version/lib/byline.js',
+              import.meta.url
+            )
+          )
+        }
     },
+  },
+  optimizeDeps: {
+    include: [
+      'util', 'events'
+    ]
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(version),
+    __APP_COPYRIGHT__: JSON.stringify(
+      description.indexOf('(C)') !== -1
+        ? description.substring(description.indexOf('(C)') + 3).trim()
+        : '2020- P4mohnet and JSGOE'
+    ),
+    'process.env': {}
   },
   build: {
     rollupOptions: {
@@ -30,3 +73,4 @@ export default defineConfig({
     environment: 'jsdom'
   }
 })
+
