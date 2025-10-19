505c2d4d518bf2990fad8bc92ac6cb56fc23aa75
Yasuhiro Yamamoto <mohyama@mohnet.com>
2025-10-20 00:50:11 +0900

    Approach実装中(WIP)

---
+++ src/components/Molecules/ApproachItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- カテゴリkeyごとにgroup化する ボタンイベントは全て編集 -->
+  <el-button-group style="margin-right: 0.8rem;">
+    <el-button type="primary" size="small" round>通常ポート配置</el-button>
+  </el-button-group>
+  <el-button-group style="margin-right: 0.8rem;">
+    <el-button type="primary" size="small" round>通常ポート配置</el-button>
+  </el-button-group>
+  <el-button-group style="margin-right: 0.8rem;">
+    <el-button type="primary" size="small" round>細径子宮鏡</el-button>
+    <el-button type="primary" size="small" round>非電解質溶液使用</el-button>
+  </el-button-group>
+</template>
+
