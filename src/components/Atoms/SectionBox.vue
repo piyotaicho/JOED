2918ad48ac9c6b160d8943a0e3ed7314d401ca31
Yasuhiro Yamamoto <mohyama@mohnet.com>
2025-11-09 17:18:44 +0900

    SectionBlockをBoxと機能に分離

---
+++ src/components/Atoms/SectionBox.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="section">
+    <div>
+      <span class="section-title">{{props.title}} ： </span>
+      <slot name="aftertitle"></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="sass">
+div.section
+  position: relative
+  background: white
+  width: 800px
+  min-height: 4rem
+  border: 1px solid black
+  margin-top: 0.85rem
+  padding-bottom: 0.35rem
+  .section-title
+    margin: 4px
+    font-size: 1.15rem
+</style>
