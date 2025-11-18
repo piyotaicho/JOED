365074e674267be6a18aa2c3be7e65b78241607c
Yasuhiro Yamamoto <mohyama@mohnet.com>
2025-11-18 14:44:10 +0900

    ツールチップを廃止

--- src/components/Atoms/DrawerButton.vue
+++ src/components/Atoms/DrawerButton.vue
@@ -20,7 +20,7 @@ const click = (event) => emit('click', event)
     @click="click"
     @keydown.enter="click"
     :accesskey="props.accesskey"
-    data-tooltip="表示設定">
+  >
     <el-icon><Operation /></el-icon>
   </div>
 </template>
@@ -37,22 +37,6 @@ div.drawer-button
     color: var(--color-text-regular)
   &:focus
     color: var(--color-text-regular)
-  &::after
-    content: attr(data-tooltip)
-    position: absolute
-    bottom: 25%
-    right: 50%
-    transform: translateX(50%)
-    color: var(--color-text-regular)
-    background-color: rgba(255,255,255,0.8)
-    padding: 0
-    font-size: 0.8rem
-    white-space: nowrap
-    opacity: 0
-    pointer-events: none
-    transition: opacity 0.5s 1s ease-in-out
-  &:hover::after
-    opacity: 1
   & > i
     width: 100%
     height: 100%
